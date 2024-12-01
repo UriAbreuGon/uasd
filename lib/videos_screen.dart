5b328ce5bbef9ba0c7cb53d5706f17261afc5cbe
Gabriel0207 <gabrielfelipe101515@gmail.com>
2024-12-01 18:22:58 -0400

    Correccion final videos_app a la rama videos

---
+++ lib/videos_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class VideosScreen extends StatefulWidget {
+  @override
+  _VideosScreenState createState() => _VideosScreenState();
+}
+
+class _VideosScreenState extends State<VideosScreen> {
+  List videos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    obtenerVideos();
+  }
+
+  Future<void> obtenerVideos() async {
+    final url = 'https://uasdapi.ia3x.com/videos';
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM2IiwibmJmIjoxNzMyODM2MzY0LCJleHAiOjE3MzI4Mzk5NjQsImlhdCI6MTczMjgzNjM2NH0.mjO8rinkc68gEmmzA9uDXCqGJLUKWKHu70dswuf0sG0'
+      },
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        videos = json.decode(response.body);
+      });
+    } else {
+      print('Error al obtener videos: ${response.statusCode}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Videos Educativos'),
+      ),
+      body: videos.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: videos.length,
+        itemBuilder: (context, index) {
+          final video = videos[index];
+          return ListTile(
+            title: Text(video['titulo']),
+            subtitle: Text(video['fechaPublicacion']),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => VideoPlayerScreen(video['url']),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class VideoPlayerScreen extends StatefulWidget {
+  final String videoUrl;
+
+  VideoPlayerScreen(this.videoUrl);
+
+  @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = YoutubePlayerController(
+      initialVideoId: widget.videoUrl,
+      flags: YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reproduciendo Video'),
+      ),
+      body: YoutubePlayer(
+        controller: _controller,
+        showVideoProgressIndicator: true,
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:uasd/main.dart';
+import 'package:uasd_pre_videos/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
